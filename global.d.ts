b1008e117a73c6982be38588a9ba35d7ae2b4087
student <student@safebear.co.uk>
2019-10-01 10:41:02 +0100

    Adding global.d.ts file

---
+++ global.d.ts
@@ -0,0 +1,20 @@
+//Type definitions for ProductList Test Automation Framework
+
+declare namespace myLib {
+
+    //Our global 'Product' type/interface
+    interface Product{
+        name: string;
+        description: string;
+        price: string;
+    }
+
+    //another example, different file types
+    /*interface User{
+
+        name: string;
+        age:number;
+        isMarried: boolean;
+    }*/
+
+}
